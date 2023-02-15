01bfe45926af3f694c7c905f23084d4091e85621
DenisovaAlina00 <113889282+DenisovaAlina00@users.noreply.github.com>
2023-02-15 10:35:15 +0300

    Add files via upload

---
+++ ConsoleApp20/ConsoleApp20/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+public class MainClass
+{
+    public static void Main()
+    {
+        int m;
+        int q;
+        int n;
+        int sum = 0;
+        Console.WriteLine("m:");
+        m = int.Parse(Console.ReadLine());
+        Console.WriteLine("q:");
+        q = int.Parse(Console.ReadLine());
+        Console.WriteLine("длина массива:");
+        n= int.Parse(Console.ReadLine());
+        int[] nums = new int[n];
+        var random = new Random();
+        for (int i = 0; i < nums.Length; i++) 
+        {
+            nums[i]= random.Next(0,1000);
+            sum += nums[i];
+            break;
+        }
+        if (m>=sum && q<sum)
+        {
+            Console.WriteLine("Истина:");
+        }
+        else
+        {
+            Console.WriteLine("Ложь:");
+        }
+    }
+}
